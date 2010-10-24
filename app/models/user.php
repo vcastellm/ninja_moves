fa1a1074663e49a528320756c94b09255784cdc2
Victor Castell <victorcoder@gmail.com>
2010-10-24 21:56:31 +0200

    Added user model

---
+++ app/models/user.php
@@ -0,0 +1,28 @@
+<?php
+class User extends AppModel {
+	var $name = 'User';
+	var $displayField = 'facebook_id';
+	var $validate = array(
+		'id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'facebook_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
+?>
---
+++ app/tests/cases/models/user.test.php
@@ -0,0 +1,18 @@
+<?php
+/* User Test cases generated on: 2010-10-24 19:10:05 : 1287949025*/
+App::import('Model', 'User');
+
+class UserTestCase extends CakeTestCase {
+	var $fixtures = array('app.user');
+
+	function startTest() {
+		$this->User =& ClassRegistry::init('User');
+	}
+
+	function endTest() {
+		unset($this->User);
+		ClassRegistry::flush();
+	}
+
+}
+?>
